243a2f02ae14b963bafbad2ab676e7fb53badba7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 17:11:41 +0530

    Add : polymorphism example..

---
+++ OOPProblem/OOPProblem/Polymorphism.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+    //Overloding..............................
+    public class MathHelper
+    {
+        public  int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public  double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public  int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+
+    //Overriding
+    class Onimal
+    {
+        public virtual void Eat()
+        {
+            Console.WriteLine("The animal is eating");
+        }
+    }
+
+    class Lion : Onimal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("The lion is eating meat");
+        }
+    }
+
+    class Giraffe : Onimal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("The giraffe is eating leaves");
+        }
+    }
+
+
+
+
+
+}
+
+
--- OOPProblem/OOPProblem/Program.cs
+++ OOPProblem/OOPProblem/Program.cs
@@ -5,10 +5,27 @@
         public static void Main(string[] args)
         {
             //Abstraction..................
-            Uog obj = new Uog();
-            obj.makeSound();
+            //Uog obj = new Uog();
+            //obj.makeSound();
 
-            Console.ReadLine();
-         }
+            //Polymorphism.................
+            //Overloding.........................
+            MathHelper overloding= new MathHelper();
+            Console.WriteLine(overloding.Add(5, 6));
+            Console.WriteLine(overloding.Add(5.3,6.6));
+            Console.WriteLine(overloding.Add(5, 6, 7));
+            //Overriding....
+            Giraffe overriding= new Giraffe();
+            overriding.Eat();
+            Lion overr = new Lion();
+            overr.Eat();
+    
+
+
+
+
+
+
+        }
     }
 }
