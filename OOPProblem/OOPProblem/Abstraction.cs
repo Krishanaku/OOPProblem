e971f2f4256a05797f8bb5f561f1a1f1906c080e
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 16:46:51 +0530

    Add : Abstraction Example

---
+++ OOPProblem/OOPProblem/Abstraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+    abstract class Unimal
+    {
+
+        // abstract method
+        public abstract void makeSound();
+    }
+
+    // inheriting from abstract class
+    class Uog : Unimal
+    {
+
+        // provide implementation of abstract method
+        public override void makeSound()
+        {
+
+            Console.WriteLine("Bark Bark");
+
+        }
+
+    }
+}
--- OOPProblem/OOPProblem/Program.cs
+++ OOPProblem/OOPProblem/Program.cs
@@ -4,43 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            //Inheritance.........
-            //SuperStar Inheri = new SuperStar();
-            //Inheri.Haritik();
-            //Inheri.BollyModel();
+            //Abstraction..................
+            Uog obj = new Uog();
+            obj.makeSound();
 
-            //Multilevel inheritance.....
-            //Dog myDog = new Dog();
-            //myDog.Eat();
-            //myDog.Walk();
-            //myDog.Bark();
-
-            //hierarchical inheritance.........
-            //ChildFirst first = new ChildFirst();
-            //Console.WriteLine(first.FatherName());
-            //Console.WriteLine(first.ChildDName());
-
-            //ChildSecond second = new ChildSecond();
-            //Console.WriteLine(first.FatherName());
-            //Console.WriteLine(first.ChildDName());
-            //Hybrid Inheritance....................
-            //Cylinder obj = new Cylinder();
-            //obj.Display();
-            //obj.Draw();
-            //obj.Fill();
-            //Console.ReadKey();
-            //Interface..................................................
-            IAnimal myDog = new Dogs();
-            IAnimal myCat = new Cat();
-
-            myDog.MakeSound();  
-            Console.WriteLine(myDog.GetSpecies()); 
-
-            myCat.MakeSound();  
-            Console.WriteLine(myCat.GetSpecies());  
-
-
-
-        }
+            Console.ReadLine();
+         }
     }
 }
