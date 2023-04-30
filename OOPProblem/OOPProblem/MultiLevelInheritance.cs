0ac23e0d5452aa9d2a4ba02727588fc54679990a
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 15:50:02 +0530

    Add : Mutilevel Inheritance Example..

---
+++ OOPProblem/OOPProblem/MultiLevelInheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+        public class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("The animal is eating.");
+            }
+        }
+
+        public class Mammal : Animal
+        {
+            public void Walk()
+            {
+                Console.WriteLine("The mammal is walking.");
+            }
+        }
+
+        public class Dog : Mammal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("The dog is barking.");
+            }
+        }
+ }
+
--- OOPProblem/OOPProblem/Program.cs
+++ OOPProblem/OOPProblem/Program.cs
@@ -4,31 +4,16 @@
     {
         public static void Main(string[] args)
         {
-            
+            //Inheritance.........
+            SuperStar Inheri = new SuperStar();
+            Inheri.Haritik();
+            Inheri.BollyModel();
 
-            //Value And Reference.......................
-            ValueAndReference obj= new ValueAndReference();
-            obj.valueType(); 
-            //Reference..................................
-            Person person_One= new Person();
-            
-            Person person_Two = person_One; // Creates a reference to person1
-            Console.WriteLine(person_One.Name ="Krrish");
-            Console.WriteLine(person_One.Age = 30);
-            Console.WriteLine(person_Two.Age = 35);
-
-            // create a new instance of the Person class
-            Person person1 = new Person { Name = "John", Age = 30 };
-
-            // assign the reference to the person1 object to person2
-            Person person2 = person1;
-
-            // update the name property of the person2 object
-            person2.Name = "Jane";
-
-            // the name property of the person1 object is also updated
-            Console.WriteLine(person1.Name); // output: Jane
-
+            //Multilevel inheritance.....
+            Dog myDog = new Dog();
+            myDog.Eat();
+            myDog.Walk();
+            myDog.Bark();
 
 
         }
