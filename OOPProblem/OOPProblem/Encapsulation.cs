a75e1b7af050f776b47a73d67af43c1c33c90650
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 17:21:26 +0530

    Add : Encapsulation Example...

---
+++ OOPProblem/OOPProblem/Encapsulation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+    public class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public BankAccount(string accountNumber, double initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+
+    public class Persou
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+
+}
--- OOPProblem/OOPProblem/Program.cs
+++ OOPProblem/OOPProblem/Program.cs
@@ -10,16 +10,37 @@
 
             //Polymorphism.................
             //Overloding.........................
-            MathHelper overloding= new MathHelper();
-            Console.WriteLine(overloding.Add(5, 6));
-            Console.WriteLine(overloding.Add(5.3,6.6));
-            Console.WriteLine(overloding.Add(5, 6, 7));
-            //Overriding....
-            Giraffe overriding= new Giraffe();
-            overriding.Eat();
-            Lion overr = new Lion();
-            overr.Eat();
-    
+            //MathHelper overloding= new MathHelper();
+            //Console.WriteLine(overloding.Add(5, 6));
+            //Console.WriteLine(overloding.Add(5.3,6.6));
+            //Console.WriteLine(overloding.Add(5, 6, 7));
+            ////Overriding....
+            //Giraffe overriding= new Giraffe();
+            //overriding.Eat();
+            //Lion overr = new Lion();
+            //overr.Eat();
+            BankAccount myAccount = new BankAccount("123456789", 1000);
+            myAccount.Deposit(500);
+            Console.WriteLine(myAccount.GetBalance());   
+
+            bool success = myAccount.Withdraw(2000);
+            if (success)
+            {
+                Console.WriteLine("Withdrawal successful");
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal failed");
+            }
+            Console.WriteLine(myAccount.GetBalance());
+
+
+            Persou person1 = new Persou();
+            person1.Name = "John Doe";
+            person1.Age = 30;
+            Console.WriteLine($"Name: {person1.Name}, Age: {person1.Age}");
+
+
 
 
 
