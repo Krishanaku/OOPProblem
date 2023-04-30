2baf3177dc0faeaaa0aa7e6a4bdd5e96b2c18171
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 11:34:43 +0530

    Add : Class Object Example ...

---
+++ OOPProblem/OOPProblem/ClassName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+    public  class ClassName
+    {
+        //Non Parameterized 
+        public void sum()
+        {
+            Console.WriteLine("Enter Two Number :");
+            int firstNum=Convert.ToInt32(Console.ReadLine());
+            int secNum=int.Parse(Console.ReadLine());
+            int cal = firstNum + secNum;
+            Console.WriteLine("Two number sum : "+cal);
+        }
+        //Parameterized method
+        public void sub(int firstVal , int secVal)
+        {
+            int cal = firstVal - secVal;
+            Console.WriteLine("Two number sub : " + cal);
+        }
+         //returnType method..... 
+        public int mul(int firstVal , int secVal)
+        {
+            int cal = firstVal * secVal;
+            return cal;
+            
+
+        }
+    }
+}
---
+++ OOPProblem/OOPProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOPProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("OOP Concept Problem.....");
+            //Method.................................................
+            ClassName method= new ClassName(); // object
+            method.sum();
+            method.sub(56,40);
+            int multi=method.mul(25, 25);
+            Console.WriteLine("Multi of two number : "+multi);
+            Console.ReadLine();
+        }
+    }
+}
