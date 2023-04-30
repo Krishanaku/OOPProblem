49d85b6b90d750709a73b359636a011a18475164
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 13:15:48 +0530

    Add : Enum Example...

---
+++ OOPProblem/OOPProblem/EnumType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+    //Enum stand for Enumeration....
+    //Enum is special class that represents a group of constant(unchangeable/read-only variable)
+    // enum value set when assign never change....
+    public class EnumType
+    {
+    }
+    enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    enum Colors
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+}
--- OOPProblem/OOPProblem/Program.cs
+++ OOPProblem/OOPProblem/Program.cs
@@ -15,11 +15,30 @@
             //Console.ReadLine();
 
             //Constructor................................
-            ConstructType construct = new ConstructType("Mustang", "Red", 1969);
-            ConstructType construct1 = new ConstructType(1001, "Shine");
-            construct1.DisplayConstructor();
-            Console.WriteLine(construct.color + " " + construct.year + " " + construct.model);
+            //ConstructType construct = new ConstructType("Mustang", "Red", 1969);
+            //ConstructType construct1 = new ConstructType(1001, "Shine");
+            //construct1.DisplayConstructor();
+            //Console.WriteLine(construct.color + " " + construct.year + " " + construct.model);
 
+            //Enum..............................
+            Console.WriteLine(DayOfWeek.Monday);
+            int count = (int)DayOfWeek.Monday;
+            Console.WriteLine(count);
+            //Second enum
+            Colors favoriteColor = Colors.Blue;
+            Console.WriteLine(favoriteColor);
+            switch (favoriteColor)
+            {
+                case Colors.Red:
+                    Console.WriteLine("Your favorite color is red!");
+                    break;
+                case Colors.Green:
+                    Console.WriteLine("Your favorite color is green.");
+                    break;
+                case Colors.Blue:
+                    Console.WriteLine("Your favorite color is blue.");
+                    break;
+            }
         }
     }
 }
