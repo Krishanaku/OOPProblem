a46f39597c3f511c5249ce25a5fec7df36e0bb36
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-04-30 16:15:35 +0530

    Add : Hybrid Inheritance Example

---
+++ OOPProblem/OOPProblem/HybridInheritance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPProblem
+{
+    class Shape
+    {
+        public void Display()
+        {
+            Console.WriteLine("Displaying shape...");
+        }
+    }
+
+    // base class 2
+    class Circle : Shape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing circle...");
+        }
+    }
+
+    // derived class
+    class Cylinder : Circle
+    {
+        public void Fill()
+        {
+            Console.WriteLine("Filling cylinder...");
+        }
+    }
+}
